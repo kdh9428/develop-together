bc984433d0615490eeffbcb4435fd8e846fe64c7
dahun <kim23222@naver.com>
2021-10-29 12:20:30 +0900

    Initial commit

---
+++ src/main/java/com/dev/togeter/dev_together/DevelopTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.dev.togeter.dev_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/togeter/dev_together/DevelopTogetherApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dev.togeter.dev_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
